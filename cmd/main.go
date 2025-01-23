1a02711e9a715ac3678e4c917c491a2dce9f43d9
Alex Kraasch <alex@kraasch.eu>
2025-01-23 02:02:34 +0100

    Fix format.

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,7 @@ import (
   "os"
   // make web request to imdb.
   "github.com/StalkR/imdb"
-	"github.com/StalkR/httpcache"
+  "github.com/StalkR/httpcache"
   "net/http"
   "time"
   // print output.
@@ -17,40 +17,40 @@ const cacheTTL = 24 * time.Hour
 var client *http.Client
 
 type customTransport struct { // customTransport implements http.RoundTripper interface to add some headers.
-	http.RoundTripper
+  http.RoundTripper
 }
 
 func (e *customTransport) RoundTrip(r *http.Request) (*http.Response, error) {
-	r.Header.Set("Accept-Language", "en") // avoid IP-based language detection
-	r.Header.Set("User-Agent", userAgent)
-	return e.RoundTripper.RoundTrip(r)
+  r.Header.Set("Accept-Language", "en") // avoid IP-based language detection
+  r.Header.Set("User-Agent", userAgent)
+  return e.RoundTripper.RoundTrip(r)
 }
 
 func AskImdb(title string) (string) {
 
   // init client.
-	if _, err := os.Stat("cache"); err == nil {
-		client, err = httpcache.NewPersistentClient("cache", cacheTTL)
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		client = httpcache.NewVolatileClient(cacheTTL, 1024)
-	}
-	client.Transport = &customTransport{client.Transport}
+  if _, err := os.Stat("cache"); err == nil {
+    client, err = httpcache.NewPersistentClient("cache", cacheTTL)
+    if err != nil {
+      panic(err)
+    }
+  } else {
+    client = httpcache.NewVolatileClient(cacheTTL, 1024)
+  }
+  client.Transport = &customTransport{client.Transport}
 
   // make web request.
   s := ""
   s += fmt.Sprintln("\n> Search title")
-	results, err := imdb.SearchTitle(client, title)
+  results, err := imdb.SearchTitle(client, title)
 
   // evaluate and print results.
-	if err != nil { // has error.
+  if err != nil { // has error.
     s += fmt.Sprintf("imdb.SearchTitle(%s) error: %v", title, err)
-	}
-	if len(results) == 0 { // no results found.
+  }
+  if len(results) == 0 { // no results found.
     s += fmt.Sprintf("imdb.SearchTitle(%s) error: %d results found.", title, len(results))
-	}
+  }
   // print results.
   s += fmt.Sprintf("%d titles found.\n", len(results))
 
